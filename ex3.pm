bf0b73592b0094dee37a9f35aaba4bd1a999d88d
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2022-05-16 01:38:59 -0300

    ejercicio 3

---
+++ ex3.pm
@@ -0,0 +1,28 @@
+use Bio::Tools::Run::Alignment::Clustalw;
+
+@config = ('outfile' => "./out-msa");
+my $factory = Bio::Tools::Run::Alignment::Clustalw->new(@config);
+
+@seq_array =();
+
+push_sequence("./msa-in/P49769.1.fasta");
+push_sequence("./msa-in/P79802.1.fasta");
+push_sequence("./msa-in/P97887.1.fasta");
+push_sequence("./msa-in/Q6RH31.2.fasta");
+push_sequence("./msa-in/Q8HXW5.1.fasta");
+
+sub push_sequence {
+  my ($sequence_file) = @_;
+  
+  my $seq_obj = Bio::SeqIO->new(
+    -file => "$sequence_file",  
+    -format => 'fasta'
+  );
+  
+  while (my $seq = $seq_obj->next_seq()) {
+    push (@seq_array, $seq);
+  }
+}
+
+@sequences_ref = \@seq_array;
+my $aln = $factory->align(@sequences_ref);
